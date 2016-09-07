ae5a8fbdb41b2c334c30f4f96cc809fd72269fbf
agileabc <agileabc@sina.com>
2016-09-07 16:13:07 +0800

    Add something.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 
-OKOK
+### How to check style?
+
+jscheckstyle src/hello.js
+
+### Asynchronous Test
+
+https://github.com/javascript-me/first-server/blob/master/test/indexTest.js
 
--- test/mock-test.js
+++ test/mock-test.js
@@ -1,9 +1,9 @@
-var assert = require('assert');
 var expect = require('expect.js');
 
 it('should equal', function () {
-    assert.equal(20, 20);
     expect({ a: 'b' }).to.eql({ a: 'b' });
+    expect(1).to.be(1);
+    expect(NaN).not.to.equal(NaN);
+    expect(1).not.to.be(true);
+    expect('1').to.not.be(1);
 });
-
-
